c4b2996b19602a6d67209d5ff96308884bbe803b
Abourass <Abourassa@AssetVal.com>
2024-02-22 10:54:15 -0700

    Add EditorConfig and ESLint configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,37 @@
 module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: [
+      './tsconfig.json',
+    ],
+    ecmaVersion: 'latest',
+  },
   'env': {
     'browser': true,
     'commonjs': true,
-    'es2021': true
+    'es2022': true
   },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 'latest'
-  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:n/recommended',
+    'plugin:security/recommended',
+    'plugin:import/typescript',
+    'plugin:@getify/proper-ternary/getify-says',
+    'prettier',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'n',
+    'security',
+    '@getify/proper-ternary',
+  ],
   'rules': {
     'indent': [
       'error',
